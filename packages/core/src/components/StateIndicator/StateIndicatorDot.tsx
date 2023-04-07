585b37ff8b56cb8a326e48919c05b2ce64dfbdf5
bpx-network <admin@bpxcoin.cc>
2023-04-08 00:41:05 +0200

    Fix StageIndicatorDot

--- packages/core/src/components/StateIndicator/StateIndicatorDot.tsx
+++ packages/core/src/components/StateIndicator/StateIndicatorDot.tsx
@@ -1,32 +1,7 @@
-import { ConnectCheckmark, ConnectCancel, ConnectReload } from '@bpx-network/icons';
-import React from 'react';
+import { FiberManualRecord as FiberManualRecordIcon } from '@mui/icons-material';
 import styled from 'styled-components';
 
-import State from '../../constants/State';
-
-const WrapperStyled = styled.div`
-  display: inline-block;
+export default styled(FiberManualRecordIcon)`
   font-size: 1rem;
-  position: relative;
+  color: ${({ color }) => color};
 `;
-
-type StateIndicatorDotTypes = {
-  state: string;
-};
-
-export default function StateIndicatorDot(props: StateIndicatorDotTypes) {
-  const { state } = props;
-  function renderIcon() {
-    if (state === State.SUCCESS) {
-      return <ConnectCheckmark className="checkmark-icon" sx={{ width: '21px', height: '21px' }} />;
-    }
-    if (state === State.WARNING) {
-      return <ConnectReload className="reload-icon" sx={{ width: '21px', height: '21px' }} />;
-    }
-    if (state === State.ERROR) {
-      return <ConnectCancel className="cancel-icon" sx={{ width: '21px', height: '21px' }} />;
-    }
-    return null;
-  }
-  return <WrapperStyled>{renderIcon()}</WrapperStyled>;
-}
