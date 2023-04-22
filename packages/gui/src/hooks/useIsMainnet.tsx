b24448962aea952d19125043f759d990aebfcf83
bpx-network <admin@bpxcoin.cc>
2023-04-22 10:52:00 +0200

    Debug network name

--- packages/gui/src/hooks/useIsMainnet.tsx
+++ packages/gui/src/hooks/useIsMainnet.tsx
@@ -2,7 +2,9 @@ import { useGetNetworkInfoQuery } from '@bpx-network/api-react';
 
 export default function useIsMainnet(): boolean | undefined {
   const { data: networkInfo } = useGetNetworkInfoQuery();
+  console.log(networkInfo);
   const networkName = networkInfo?.networkName;
+  console.log(networkName);
 
   if (!networkName) {
     return undefined;
